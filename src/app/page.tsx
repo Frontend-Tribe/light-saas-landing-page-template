16dba04847b2e94afb554200571a6d01eef9a8e9
Joshua Cook <joshuacook@Taras-MacBook-Pro.local>
2024-07-12 05:54:04 -0500

    After call to action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { LogoTicker } from "@/sections/LogoTicker";
 import { ProductShowcase } from "@/sections/ProductShowcase";
 import { Pricing } from "@/sections/Pricing";
 import { Testimonials } from "@/sections/Testimonials";
+import { CallToAction } from "@/sections/CallToAction";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <ProductShowcase />
       <Pricing />
       <Testimonials />
+      <CallToAction />
     </>
   );
 }
