79a2f3643c46f131e97fe86695662e4cc44ff71d
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-08-15 14:26:59 -0300

    Header: component created; added to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Header } from "@/sections/Header";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,24 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from "@/assets/arrow-right.svg";
+import Logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0 backdrop-blur-sm z-20">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">
+          Streamline your workflow and boost your productivity
+        </p>
+        <div className="inline-flex gap-1 items-center">
+          <p>Get started for free</p>
+          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
+        </div>
+      </div>
+      <div className="py-5 container flex items-center justify-between">
+        <Image src={Logo} alt="Saas logo" height={40} width={40} />
+        <Navbar />
+      </div>
+    </header>
+  );
 };
